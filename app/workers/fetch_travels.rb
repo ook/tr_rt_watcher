39d1fca8110c6c79fa854251304cc666d5e3ef7f
Thomas Lecavelier <thomas@followanalytics.com>
2015-06-29 14:34:22 +0200

    Erm… forgotten the big file

---
+++ app/workers/fetch_travels.rb
@@ -0,0 +1,21 @@
+require 'transilien_realtime'
+class FetchTravels
+  include Sidekiq::Worker
+
+  MANDATORY_ENV = %w(RTT_API_USER RTT_API_PWD)
+  def setup_worker
+    @user, @pwd = *MANDATORY_ENV.map { |key| ENV[key] }
+    raise ArgumentError, "ENV var: #{MANDATORY_ENV} are mandatory" unless @user && @pwd
+    @tr = TransilienRealtime::Base.new(user: @user, pwd: @pwd)
+  end
+
+  def perform(from: '87384008', to: '87381798')
+    setup_worker
+    @tr.next(from: from, to: to)
+    @tr.trains.each do |train|
+      t = Travel.add_travel(train)
+      puts t
+    end
+    FetchTravels.perform_in(1.minute)
+  end
+end
