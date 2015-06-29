965970835ed5e98b5e0f2bb3751f89ab8420e7d2
Thomas Lecavelier <thomas@followanalytics.com>
2015-06-29 14:28:02 +0200

    Travel.add_travel

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,2 +1,17 @@
 class Travel < ActiveRecord::Base
+
+  class << self
+    def add_travel(train)
+      travel  = Travel.where(num: train.numero).first
+      unless travel
+        travel = new
+        travel.num     = train.numero
+        travel.mission = train.mission
+        travel.term    = train.terminus
+      end
+      travel.times << train.departure_at
+      travel.save!
+      travel
+    end
+  end
 end
