fbfbe92af8647aa7309f44ec9b93a69b2e5f1e40
Thomas Lecavelier <thomas@followanalytics.com>
2015-10-22 23:22:00 +0200

    Ensure we have only 1 worker for a given configuration running every EXPIRE_DELAY seconds (30 at commit time)

--- app/workers/fetch_travels.rb
+++ app/workers/fetch_travels.rb
@@ -3,15 +3,45 @@ class FetchTravels
   include Sidekiq::Worker
 
   MANDATORY_ENV = %w(RTT_API_USER RTT_API_PWD)
+  def redis
+    @redis ||= TrRtWatcher.redis
+  end
+
   def setup_worker
     @user, @pwd = *MANDATORY_ENV.map { |key| ENV[key] }
     raise ArgumentError, "ENV var: #{MANDATORY_ENV} are mandatory" unless @user && @pwd
     @tr = TransilienRealtime::Base.new(user: @user, pwd: @pwd)
   end
 
+  def worker_key
+    @worker_key ||= "#{self.class.name}-#{@from}-#{@to||'nil'}"
+  end
+
+  EXPIRE_DELAY = 30.second
+  def track_in_redis
+    milli = redis.get(worker_key).to_i
+    return :must_stop if milli != 0 && Time.now.to_i <= milli
+    redis.watch(worker_key) do
+      multi_ret = redis.multi do
+        redis.set(worker_key, Time.now.to_i + EXPIRE_DELAY)
+      end
+      return :must_stop if multi_ret.nil?
+    end
+  end
+
+  def clean_redis
+    redis.del(worker_key)
+  end
+
   #def perform(from: '87381798', to: '87384008')
   def perform(from: '87381798', to: nil)
     setup_worker
+    @from = from
+    @to = to
+    if :must_stop == track_in_redis # End tje worker right now
+      logger.warn("Discover that we MUST STOP. Certainly a duplicated request.")
+      return true
+    end
     #@tr.next(from: from, to: to)
     @tr.next(from: from)
     @tr.trains.each do |train|
